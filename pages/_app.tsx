dc534a171c9611bd87bf74ca69bb1a1dff644b55
stvmachine <campos.esteban@gmail.com>
2022-08-17 15:37:25 +1000

    init repo with some progress already

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
+import resetStyles from "../styles/reset";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Global
+        styles={css`
+          ${resetStyles}
+        `}
+      />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/reset.ts
@@ -0,0 +1,26 @@
+import { css } from "@emotion/react";
+
+const resetCss = css`
+  body {
+    margin: 0;
+  }
+
+  a,
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    outline: 0;
+  }
+
+  button:focus,
+  button:active,
+  canvas:focus,
+  canvas:active {
+    outline: none;
+  }
+`;
+
+export default resetCss;
